280dd2f00ca2dde44c80b2b2ecee84170a239dc3
nmkhoi3006 <khoi30062005@gmail.com>
2025-05-10 21:04:08 +0700

    merge

---
+++ resources/views/course/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('layouts.head')
+</head>
+<body>
+    <!-- header -->
+   @include('layouts.sections.our_courses')
+    <!-- footer -->
+    @include('layouts.footer')
+</body>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Container\Attributes\Auth;
 
 Route::get('/', function () {
@@ -21,4 +22,6 @@ Route::get('/signup/show', function(){
 Route::get('/signin', [AuthController::class, 'signin'])->name('auth.signin');
 
 Route::get('/signup', [AuthController::class, 'signup'])->name('auth.signup');
+
+Route::resource('course', CourseController::class);
 
