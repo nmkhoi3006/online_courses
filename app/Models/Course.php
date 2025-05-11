80328043a770a9eafc00398aa406197c56ca5fb0
nmkhoi3006 <khoi30062005@gmail.com>
2025-05-11 10:46:54 +0700

    local query

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Course extends Model
 {
@@ -12,5 +13,42 @@ class Course extends Model
 
     public function reviews(){
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title)
+    {
+        return $query->where('title', 'like', "%$title%");
+    }
+
+    public function scopePopular(Builder $query)
+    {
+        return $query->withCount('reviews')
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRating(Builder $query)
+    {
+        return $query->withAvg('reviews', 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeLowestPrice(Builder $query)
+    {
+        return $query->orderBy('price', 'asc');
+    }
+
+    public function scopeHighestPrice(Builder $query)
+    {
+        return $query->orderBy('price', 'desc');
+    }
+
+    public function scopeCategory(Builder $query, string $category)
+    {
+        return $query->where('category', 'like', "%$category%");
+    }
+    
+    public function scopeLevel(Builder $query, string $level)
+    {
+        return $query->where('level', 'like', "%$level%");
     }
 }
